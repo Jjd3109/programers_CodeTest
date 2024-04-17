08c7c5af4551af96e4a14cffd41752186ae32c36
정종두 <jongdoo2001@naver.com>
2024-04-17 15:34:16 +0900

    <해결> 짝수와 홀수 / 12944 / Lv2

---
+++ src/programers/Lessons12937.java
@@ -0,0 +1,13 @@
+package programers;
+
+import java.util.Stack;
+
+public class Lessons12937 {
+
+    public String solution(int num) {
+        if(num % 2 == 0) {
+            return ("Even");
+        } else
+            return ("Odd");
+    }
+}
